893e250d3da8cc011368ea08cef53a6d4d20a4fa
csdojo <sherrywanwang@gmail.com>
2018-08-14 13:31:21 -0400

    comment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,10 +18,9 @@ var letterToGuess = null;
 // get computerChoice
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-//Allows the user 9 guesses
-// guesses = guesses || 9
+
 function updateGuessesLeft() {
-    // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+
     document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
@@ -30,11 +29,11 @@ function updateLetterToGuess() {
 };
 
 function updateGuessesSoFar() {
-    // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+    
     document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
 
-// Function will be called when we reset everything
+// reset
 var reset = function() {
     totalGuesses = 9;
     guessesLeft = 9;
@@ -48,7 +47,7 @@ var reset = function() {
 updateLetterToGuess();
 updateGuessesLeft();
 
-//When key is released it becomes the users guess
+
 document.onkeyup = function(event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     var check = computerChoices.includes(userGuess);
@@ -57,7 +56,7 @@ document.onkeyup = function(event) {
         alert("That was not a valid guess, try again?");
         return false;
     } else if (check === true) {
-        //If the Users choice was an alphabet char then update guesses left and add users guess to the array of guessed letters
+       
         guessesLeft--;
         guessedLetters.push(userGuess);
         updateGuessesLeft();
@@ -72,11 +71,11 @@ document.onkeyup = function(event) {
                 reset();
             }
         } else if (guessesLeft == 0) {
-            // Then we will loss and we'll update the html to display the loss 
+          
             losses++;
             document.querySelector('#losses').innerHTML = "Losses: " + losses;
             alert("Sorry, you're not psychic, maybe try again?");
-            // Then we'll call the reset. 
+    
             reset();
         }
         return false;
